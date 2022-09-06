743e5ce89ca2b14a50b3bd9b8e4b5440efe029f8
Beef <beefers@riseup.net>
2022-09-06 20:33:58 +0100

    [Styling] Hopefully the last of the styling changes...

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,7 @@
 import uno from "astro-uno";
 import config from "./uno.config";
 import { presetUno, presetWind } from "unocss";
+import { presetScrollbar } from "unocss-preset-scrollbar";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
@@ -8,7 +9,7 @@ export default defineConfig({
     site: "https://beefers.dev",
     integrations: [
         uno({
-            presets: [presetUno(), presetWind()],
+            presets: [presetUno(), presetWind(), presetScrollbar()],
             ...config,
         }),
     ],
